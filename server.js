2f67d3314e786958a6a4c8ed046937c052fe3619
Chad Smith <cds021@yahoo.com>
2023-09-12 11:44:58 -0400

    add server.js and gitignore file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.port || 3001;
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
