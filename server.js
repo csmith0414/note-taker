b23a5d23ca8a2e88dcece83c2e98c1784d15ede7
Chad Smith <cds021@yahoo.com>
2023-09-18 11:52:05 -0400

    updated js files

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
+// set up app use express
 const app = express();
 
+// set up ports
 const PORT = process.env.port || 3001;
 
+// setting up static to public folder
+app.use(express.static('public'));
+
+// sets up middle wear
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// routes to files
+require('./routes/api')(app);
+require('./routes/html')(app);
+
+// app listener
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
